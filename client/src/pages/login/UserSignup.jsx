161f6057daf36ab8a7d8277924367b07c8f4e9e5
Clarissa Mobley <clarissamobley@me.com>
2024-07-23 10:06:14 -0400

    added User signup

---
+++ client/src/pages/login/UserSignup.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const UserSignup = () => {
+    return (
+        <div>
+            <Link to='/login'>Login</Link>
+        </div>
+    )
+}
+
+export default UserSignup
