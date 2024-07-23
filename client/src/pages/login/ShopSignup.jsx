a858834510acbf0c624edf36096e23f494816b85
Clarissa Mobley <clarissamobley@me.com>
2024-07-23 10:04:33 -0400

    added to shopsignup page

---
+++ client/src/pages/login/ShopSignup.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const ShopSignup = () => {
+    return (
+        <div>
+            <Link to='/login'>Login</Link>
+        </div>
+    )
+}
+
+export default ShopSignup
