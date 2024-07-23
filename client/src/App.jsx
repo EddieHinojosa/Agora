c6bd001b29af51c6d0240e76a7d5c460a6576fd2
Clarissa Mobley <clarissamobley@me.com>
2024-07-22 23:13:07 -0400

    added new files, added to main.jsx, added to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import LoginApp from './LoginApp.jsx';
+import ShopApp from './ShopApp.jsx';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Error from "./pages/Error.jsx";
+
+import Home from "./pages/Home.jsx";
+import User from "./pages/User.jsx";
+import Cart from "./pages/Cart.jsx";
+
+import Login from "./pages/login/Login.jsx";
+import ShopSignup from "./pages/login/ShopSignup.jsx";
+import UserSignup from "./pages/login/UserSignup.jsx";
+
+import ShopManager from "./pages/shop-manager/ShopManager.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'user',
+        element: <User />
+      },
+      {
+        path: 'cart',
+        element: <Cart />
+      }
+    ],
+    errorElement: <Error />
+  },
+  {
+    path: 'login/*',
+    element: <LoginApp />,
+    children: [
+      {
+        index: true,
+        element: <Login />
+      },
+      {
+        path: 'usersignup',
+        element: <UserSignup />
+      }
+    ], 
+    errorElement: <Error />
+  },
+  {
+    path: 'shopmanager/*',
+    element: <ShopApp />,
+    children: [
+      {
+        index: true,
+        element: <ShopManager />
+      }
+    ], 
+    errorElement: <Error />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
