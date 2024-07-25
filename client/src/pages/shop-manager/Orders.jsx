605d5b15a004a3f33c616302d633e50d730f6e59
Clarissa Mobley <clarissamobley@me.com>
2024-07-25 11:41:02 -0400

    added processing time to shipping section and added an order page structure

--- client/src/pages/shop-manager/Orders.jsx
+++ client/src/pages/shop-manager/Orders.jsx
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom'
+import { IoMdSearch } from "react-icons/io";
+
 const Orders = () => {
     return (
-        <div></div>
+        <div><div className="container mx-auto p-4">
+        <div className="flex items-center mb-4">
+            <span className="text-2xl font-bold">Orders</span>
+        </div>
+        <div className="flex justify-end">
+            <div className="w-full bg-gray-100 flex p-1 space-x-4">
+                <Link to="#" className="p-2 hover:underline">New</Link>
+                <Link to="#" className="p-2 hover:underline">Completed</Link>
+            </div>
+        </div>
+    </div></div>
     )
 }
 
