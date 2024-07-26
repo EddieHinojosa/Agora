e8a54bce473dd21d3db873c814cea6d338e1dc79
eddie Hinojosa <eddie_hinojosa@live.com>
2024-07-26 04:30:16 -0400

    added cart modal

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,11 +1,48 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-    return (
-        <div className='text-center'>
-            <Link to="/checkout" className='hover:underline'>Checkout</Link>
+  return (
+    <div className="fixed inset-0 z-50 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+        <div
+          className="fixed inset-0 transition-opacity"
+          aria-hidden="true"
+        >
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
-    )
-}
 
-export default Cart
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+        <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">
+                  Shopping Cart
+                </h3>
+                <div className="mt-2">
+                  <h1>1. clarissa's socks</h1>
+                  <h1>2. devon's hawaiian shirts</h1>
+                  <h1>3. Alex's pants</h1>
+
+                {/* somewhere here, checkout button to redirect to stripe */}
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <Link
+              to="/"
+              className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-500 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Close
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
