1c8958262016f4d12d7b0924d42a571fc41c344f
Clarissa Mobley <clarissamobley@me.com>
2024-07-26 22:55:36 -0400

    port change

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import shopRoutes from './routes/shop.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 3000; 
 
 // Enable CORS
 app.use(cors({
