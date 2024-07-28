636bb7a503917d9b9744831c98472e630f2dc524
Clarissa Mobley <clarissamobley@me.com>
2024-07-28 14:08:19 -0400

    added banner to homepage

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-    <footer className="p-2 text-center w-full">
+    <footer className="p-2 text-center w-full text-sm">
         <p>&copy; 2024 Agora. All rights reserved.</p>
     </footer>
 );
