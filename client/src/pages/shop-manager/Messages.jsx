6d2e0bf414cd451acbc3bb831c92bea9f8d07531
Clarissa Mobley <clarissamobley@me.com>
2024-07-28 20:37:56 -0400

    added simple message code

--- client/src/pages/shop-manager/Messages.jsx
+++ client/src/pages/shop-manager/Messages.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import sendMessage from "../../sendMessage";
 
 const Messages = () => {
+  const [recipientId, setRecipientId] = useState('');
+  const [text, setText] = useState('');
+
+  const handleSend = async () => {
+    if (recipientId && text) {
+      await sendMessage(recipientId, text);
+      setText('');
+    }
+  }
+
   return (
     <div>
-      <h1>Messages</h1>
-      {/* Your messaging UI here */}
+      <h2>Messages</h2>
+      <input type="text" placeholder="User ID" value={recipientId} onChange={(e) => setRecipientId(e.target.value)}/>
+      <textarea placeholder="Message" value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={handleSend}>Send</button>
     </div>
   );
 };
