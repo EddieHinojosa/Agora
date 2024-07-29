a64c210c6f6eee3f790ffca474abf8d51411696b
Clarissa Mobley <clarissamobley@me.com>
2024-07-28 20:23:53 -0400

    send message code

---
+++ client/src/sendMessage.js
@@ -0,0 +1,21 @@
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"
+import { auth, db } from "./firebaseConfig"
+
+const sendMessage = async (recipientId, text) => {
+    try {
+        const user = auth.currentUser;
+        if (!user) throw new Error("not authenticated")
+
+        await addDoc(collection(db, "messages") {
+            sender: user.uid,
+            recipient: recipientId,
+            text: text,
+            timestamp: serverTimestamp(),
+        })
+        console.log("Message sent!");
+    } catch (error) {
+        console.log("Error sending message:", error);
+    }
+}
+
+export default sendMessage
