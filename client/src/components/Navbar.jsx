a8922d230bb261cb456177637f539b2c8b9ec94c
Clarissa Mobley <clarissamobley@me.com>
2024-07-30 10:24:36 -0400

    categories go to menu bar on smaller screens, trying to get the other links to do that as well

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = ({ setModalIsOpen }) => {
             </button>
           </div>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className={`flex items-center space-x-4 md:flex-row md:space-y-0 ${isOpen ? 'flex' : 'hidden md:flex'}`}>
           {user ? (
             <>
               <button onClick={handleSellClick} className="text-sm">
@@ -101,16 +101,36 @@ const Navbar = ({ setModalIsOpen }) => {
           </button>
         </div>
       </div>
-      <div className={`flex flex-col items-center justify-center space-y-4 md:flex-row md:space-y-0 md:space-x-4 ${isOpen ? 'flex' : 'hidden md:flex'}`}>
-                <Link to='#' className='hover:underline text-sm'>Accessories</Link>
-                <Link to='#' className='hover:underline text-sm'>Art & Collectibles</Link>
-                <Link to='#' className='hover:underline text-sm'>Clothing</Link>
-                <Link to='#' className='hover:underline text-sm'>Home Decor</Link>
-                <Link to='#' className='hover:underline text-sm'>Food & Drink</Link>
-                <Link to='#' className='hover:underline text-sm'>Jewelry</Link>
-                <Link to='#' className='hover:underline text-sm'>Paper & Novelty</Link>
-                <Link to='#' className='hover:underline text-sm'>Pets</Link>
-            </div>
+      <div
+        className={`flex flex-col items-center justify-center space-y-4 md:flex-row md:space-y-0 md:space-x-4 ${
+          isOpen ? "flex" : "hidden md:flex"
+        }`}
+      >
+        <Link to="#" className="hover:underline text-sm">
+          Accessories
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Art & Collectibles
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Clothing
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Home Decor
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Food & Drink
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Jewelry
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Paper & Novelty
+        </Link>
+        <Link to="#" className="hover:underline text-sm">
+          Pets
+        </Link>
+      </div>
     </nav>
   );
 };
