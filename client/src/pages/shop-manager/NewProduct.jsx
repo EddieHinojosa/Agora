10ef19465713982525faabe0aba163cd8419ffa0
HeviDevi <devon.r.ross@gmail.com>
2024-07-30 17:57:06 -0400

    now with 0 conflicts, ya feel me?

--- client/src/pages/shop-manager/NewProduct.jsx
+++ client/src/pages/shop-manager/NewProduct.jsx
@@ -39,13 +39,6 @@ const [rows, setRows] = useState([
     console.log(updatedRows)
     console.log(name)
     updatedRows[0][name] = value;
-    console.log(updatedRows)
-    const updatedRows = [...rows];
-    console.log(updatedRows)
-    console.log(name)
-    updatedRows[0][name] = value;
-    console.log(updatedRows)
-    setRows(updatedRows);
   };
 
   const steps = [
