08c167424b72dcd3416f2e64c28c46f6eaaf7036
Clarissa Mobley <clarissamobley@me.com>
2024-07-30 21:03:17 -0400

    added product controller

---
+++ server/controllers/productController.js
@@ -0,0 +1,10 @@
+import Product from '../models/Product.js'
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
