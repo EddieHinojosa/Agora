73bc438d6964b8618aee14b5317c306920c61b06
Alexander Happel <Alex.happel90@gmail.com>
2024-08-01 21:49:20 -0400

    updated dependecies

--- client/src/utils/firebaseConfig.js
+++ client/src/utils/firebaseConfig.js
@@ -15,5 +15,21 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const checkIndexedDB = async () => {
+  try {
+    await db.enablePersistence();
+  } catch (err) {
+    if (err.code == 'failed-precondition') {
+      console.error('Multiple tabs open, persistence can only be enabled in one tab at a time.');
+    } else if (err.code == 'unimplemented') {
+      console.error('The current browser does not support all of the features required to enable persistence');
+    } else {
+      console.error('Error enabling persistence:', err);
+    }
+  }
+};
+
+checkIndexedDB();
+
 export { auth, db};
 
