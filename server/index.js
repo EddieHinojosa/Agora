124b99970863cfb1ea4ae96f3fd10ade6de2aa38
Alexander Happel <Alex.happel90@gmail.com>
2024-08-02 12:28:50 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -41,18 +41,18 @@ const allowedOrigins = [
   process.env.VITE_PROD_URL,
 ];
 
-app.use(cors({
+const corsOptions = {
   origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
   credentials: true,
-}));
+};
 
+app.use(cors(corsOptions));
 
 // Parse JSON
 app.use(express.json());
