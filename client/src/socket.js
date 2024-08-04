52a2be1c30492606299c174abdfcc63c6da94b75
Alexander Happel <Alex.happel90@gmail.com>
2024-08-04 18:43:09 -0400

    adjusting variables

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.MODE === 'production' ? import.meta.env.VITE_PROD_API_URL : import.meta.env.VITE_DEV_API_URL;
+const SOCKET_URL = import.meta.env.MODE === 'production' ? import.meta.env.VITE_PROD_APP_URL : import.meta.env.VITE_DEV_APP_URL;
 const socket = io(SOCKET_URL);
 
 export default socket;
