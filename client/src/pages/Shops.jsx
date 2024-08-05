063f165ccb4e29251763ccc936fc39f8e7e72fbf
Clarissa Mobley <clarissamobley@me.com>
2024-08-05 11:14:58 -0400

    created shop structure with card grid and info

--- client/src/pages/Shops.jsx
+++ client/src/pages/Shops.jsx
@@ -1,12 +1,21 @@
 import { useParams } from 'react-router-dom';
+import UserData from '../components/ShopManager/Main/UserData';
+import DisplayGrid from '../components/Home/DisplayGrid';
+// will also need to import product data, possibly shop data?
 
 const Shops = () => {
+  const { id } = useParams();
 
   return (
-    <div>
-      <h1>Shop: </h1>
-      {/* needs details and cards */}
-    </div>
+    <UserData
+      userId={id}
+      render={() => (
+        <div>
+          <h1>Welcome to {UserData.shopName}'s Shop!</h1>
+          <DisplayGrid />
+        </div>
+      )}
+    />
   );
 };
 
