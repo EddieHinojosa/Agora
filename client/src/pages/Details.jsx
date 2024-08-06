c9692f7fee03457eec4c1b0ec30e96cb0e312bb2
Clarissa Mobley <clarissamobley@me.com>
2024-08-06 13:40:57 -0400

    added to detail page

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [mainImage, setMainImage] = useState(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -15,6 +16,7 @@ const ProductDetails = () => {
             : import.meta.env.VITE_DEV_API_URL}/api/products/${id}`
         );
         setProduct(response.data);
+        setMainImage(response.data.image_urls[0]);
       } catch (error) {
         console.error("Error fetching product:", error);
       }
@@ -28,11 +30,30 @@ const ProductDetails = () => {
   }
 
   return (
-    <div>
-      <h2>{product.productName}</h2>
-      <img src={product.image_urls[0]} alt={product.name} />
-      <p>{product.price}</p>
-      <p>{product.user?.shopName || 'shop'}</p>
+    <div className="flex flex-col md:flex-row items-start mt-6 pl-20 pr-20">
+      <div className="w-full md:w-1/2 flex flex-col items-center ml-6">
+        <img
+          src={mainImage}
+          alt={product.productName}
+          className="w-full h-auto mb-4"
+        />
+        <div className="flex space-x-2">
+          {product.image_urls.slice(0, ).map((imageUrl, index) => (
+            <img
+              key={index}
+              src={imageUrl}
+              alt={`Thumbnail ${index + 1}`}
+              className="w-32 h-32 cursor-pointer object-cover"
+              onClick={() => setMainImage(imageUrl)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 mt-4 md:mt-0 md:ml-6">
+        <h2 className="text-2xl font-bold">{product.productName}</h2>
+        <p className="text-xl text-gray-700 mt-2">${product.price}</p>
+        <p className="text-md text-gray-500 mt-2">{product.user?.shopName || 'shop'}</p>
+      </div>
     </div>
   );
 };
