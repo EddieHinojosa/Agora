48724731cf7839e5f8bd11cbe45cbb0a01b71033
Clarissa Mobley <clarissamobley@me.com>
2024-08-12 19:40:35 -0400

    changed font size for banner

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -8,14 +8,14 @@ const Banner = () => {
 
   return (
     <div className="w-full flex flex-col md:flex-row items-center justify-center bg-[#4a000a] shadow-lg">
-      <div className="flex-1 text-center md:flex md:flex-col md:justify-center">
-        <h2 className="text-3xl md:text-4xl lg:text-5xl text-white leading-tight">
+      <div className="flex-1 p-4 text-center md:flex md:flex-col md:justify-center">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white leading-tight">
           Find something
         </h2>
-        <h2 className="text-3xl md:text-4xl lg:text-5xl text-white leading-tight">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl xxl:text-7xl text-white leading-tight">
           just as unique
         </h2>
-        <h2 className="text-3xl md:text-4xl lg:text-5xl text-white leading-tight">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white leading-tight">
           as you
         </h2>
         {!user && (
